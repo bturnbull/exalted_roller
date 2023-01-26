41bf61e270b156d3cab557e9086e1c2c30684d74
bturnbull <github@brianturnbull.com>
2023-01-26 17:45:19 -0500

    Tidy up dice pool component and highlight botch rolls

--- lib/exaulted_roller_web/live/roller_live/success_dice_pool_component.ex
+++ lib/exaulted_roller_web/live/roller_live/success_dice_pool_component.ex
@@ -7,22 +7,34 @@ defmodule ExaultedRollerWeb.RollerLive.SuccessDicePoolComponent do
   def success_dice_pool(assigns) do
     ~H"""
     <div class="p-3 bg-slate-200 odd:bg-slate-100">
-      <div><strong><%= @character %>:</strong> <%= success_string(@pool) %> <%= automatic_string(@pool) %></div>
-      <div><strong>Roll:</strong> <.success_dice_pool_roll pool={ @pool } /></div>
-      <div><%= integer_list_as_string(@pool.success) %> / <%= integer_list_as_string(@pool.double) %> / S:<%= @pool.stunt %> / W:<%= @pool.wound %></div>
+      <div>
+        <strong><%= @character %>:</strong>
+        <span class={success_string_class(@pool)}><%= success_string(@pool) %></span>
+      </div>
+      <div><strong>Roll:</strong> <.success_dice_pool_roll pool={@pool} /></div>
+      <div>
+        <%= dice_count_string(@pool) %> / <%= integer_list_as_string(@pool.success) %> / <%= integer_list_as_string(
+          @pool.double
+        ) %> / A:<%= SuccessDicePool.automatic_success_count(@pool) %> / S:<%= @pool.stunt %> / W:<%= @pool.wound %>
+      </div>
     </div>
     """
   end
 
   def success_dice_pool_roll(assigns) do
     ~H"""
-    <span>[ <%= for die <- @pool.dice do %><.success_die pool={ @pool } die={ die } /> <% end %>]</span>
+    <span>
+      [
+      <%= for die <- @pool.dice do %>
+        <.success_die pool={@pool} die={die} />
+      <% end %>]
+    </span>
     """
   end
 
   def success_die(assigns) do
     ~H"""
-    <span class={ success_die_class(@pool, @die) }><%= @die.value %></span>
+    <span class={success_die_class(@pool, @die)}><%= @die.value %></span>
     """
   end
 
@@ -33,33 +45,52 @@ defmodule ExaultedRollerWeb.RollerLive.SuccessDicePoolComponent do
   end
 
   defp success_die_success_class(class, %SuccessDicePool{} = pool, %SuccessDie{} = die) do
-    if SuccessDicePool.die_success?(pool, die), do: "font-bold text-green-600 #{class}", else: class
+    if SuccessDicePool.die_success?(pool, die),
+      do: "font-bold text-green-600 #{class}",
+      else: class
   end
 
   defp success_die_double_class(class, %SuccessDicePool{} = pool, %SuccessDie{} = die) do
-    if SuccessDicePool.die_double?(pool, die), do: "underline underline-offset-2 #{class}", else: class
+    if SuccessDicePool.die_double?(pool, die),
+      do: "underline underline-offset-2 #{class}",
+      else: class
   end
 
   defp integer_list_as_string(ints) when is_list(ints) do
     rep =
       ints
-      |> Enum.map(&(Integer.to_string(&1)))
+      |> Enum.map(&Integer.to_string(&1))
       |> Enum.join(" ")
 
     "[ #{rep} ]"
   end
 
   defp success_string(pool) do
-    case SuccessDicePool.success_count(pool) do
-      1 ->
+    case {SuccessDicePool.success_count(pool), SuccessDicePool.botch?(pool)} do
+      {0, true} ->
+        "Botch!"
+
+      {1, _} ->
         "1 Success"
 
-      count ->
+      {count, _} ->
         "#{count} Successes"
     end
   end
 
-  defp automatic_string(pool) do
-    "(#{SuccessDicePool.automatic_success_count(pool)} Automatic)"
+  defp success_string_class(pool) do
+    if SuccessDicePool.botch?(pool),
+      do: "text-red-600 font-bold",
+      else: ""
+  end
+
+  defp dice_count_string(pool) do
+    case length(pool.dice) do
+      1 ->
+        "1 Die"
+
+      count ->
+        "#{count} Dice"
+    end
   end
 end
