62729974c53230438e4f6b1c98dcf12cf187e23d
bturnbull <github@brianturnbull.com>
2023-01-28 15:03:19 -0500

    Dice interface with Ecto.Schema and EctoChangeset

---
+++ lib/exaulted_roller/dice/success_dice_pool.ex
@@ -0,0 +1,27 @@
+defmodule ExaultedRoller.Dice.SuccessDicePool do
+  defstruct dice: 1, success: [7, 8, 9, 10], double: [10], stunt: 0, wound: 0, reroll_once: [], reroll_none: []
+  @type t :: %__MODULE__{dice: pos_integer | nil, success: [1..10], double: [1..10], stunt: 0..3, wound: -4..0, reroll_once: [1..10], reroll_none: [1..10]}
+
+  import Ecto.Changeset
+
+  @spec create(Map.t()) :: {:ok, __MODULE__.t()} | {:error, Ecto.Changeset.t()}
+  def create(%{} = attrs) do
+    %__MODULE__{}
+    |> changeset(attrs)
+    |> Ecto.Changeset.apply_action(:insert)
+  end
+
+  @spec changeset(__MODULE__.t(), Map.t()) :: Ecto.Changeset.t()
+  def changeset(%__MODULE__{} = table, attrs) do
+    {table, %{dice: :integer, success: {:array, :integer}, double: {:array, :integer}, stunt: :integer, wound: :integer, reroll_once: {:array, :integer}, reroll_none: {:array, :integer}}}
+    |> cast(attrs, [:dice, :success, :double, :stunt, :wound, :reroll_once, :reroll_none ])
+    |> validate_required([:dice])
+    |> validate_number(:dice, greater_than: 0)
+    |> validate_inclusion(:stunt, 0..3, message: "must be 0 through 3")
+    |> validate_inclusion(:wound, -4..0, message: "must be -4 through 0")
+    |> validate_subset(:success, 1..10, message: "must contain only 1 through 10")
+    |> validate_subset(:double, 1..10, message: "must contain only 1 through 10")
+    |> validate_subset(:reroll_once, 1..10, message: "must contain only 1 through 10")
+    |> validate_subset(:reroll_none, 1..10, message: "must contain only 1 through 10")
+  end
+end
--- lib/exaulted_roller_web/live/roller_live.ex
+++ lib/exaulted_roller_web/live/roller_live.ex
@@ -5,7 +5,7 @@ defmodule ExaultedRollerWeb.RollerLive do
 
   alias Exaulted.SuccessDicePool
   alias ExaultedRoller.Tables
-  alias ExaultedRoller.Tables.Table
+  alias ExaultedRoller.Dice
 
   import ExaultedRollerWeb.RollerLive.SuccessDicePoolComponent
 
@@ -16,19 +16,23 @@ defmodule ExaultedRollerWeb.RollerLive do
       <.header class="text-center">
         Roller!
         <:subtitle>
-          <.link href={~p"/leave"} method="delete">Leave</.link>
+          <.link href={~p"/leave"} method="delete">Leave Table</.link>
         </:subtitle>
       </.header>
       <.simple_form
         :let={f}
-        id="roll_form"
-        for={:roll}
-        as={:roll}
+        id="roll-form"
+        for={@changeset}
+        phx-change="validate"
         phx-submit="roll"
       >
-        <.input field={{f, :dice_count}} type="text" label="Dice Count:" required />
-        <.input field={{f, :stunt}} type="text" value="0" label="Stunt:" required />
-        <.input field={{f, :wound}} type="text" value="0" label="Wound:" required />
+        <.input field={{f, :dice}} type="text" inputmode="numeric" pattern="[0-9]*" label="Dice Count:" required />
+        <.input field={{f, :stunt}} type="text" inputmode="numeric" pattern="[0-9]*" label="Stunt:" />
+        <.input field={{f, :wound}} type="text" inputmode="numeric" pattern="[-0-9]*" label="Wound:" />
+        <.input field={{f, :success}} type="select" multiple={true} options={1..10} label="Success:" />
+        <.input field={{f, :double}} type="select" multiple={true} options={[{"Clear", nil}, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} label="Double:" />
+        <.input field={{f, :reroll_once}} type="select" multiple={true} options={[{"Clear", nil}, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} label="Reroll Once:" />
+        <.input field={{f, :reroll_none}} type="select" multiple={true} options={[{"Clear", nil}, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} label="Reroll Until None:" />
         <:actions>
           <.button phx-disable-with="Rolling ..." class="w-full">
             Roll <span aria-hidden="true">→</span>
@@ -59,27 +63,66 @@ defmodule ExaultedRollerWeb.RollerLive do
     {
       :ok,
       socket
-      |> assign(:table, Tables.join(uid: socket.assigns.table.uid))
-      |> assign(:players, table_players(socket))
+      |> assign_table()
+      |> assign_players()
+      |> assign_dice_pool()
+      |> assign_changeset()
     }
   end
 
   @impl true
-  def handle_event("roll", %{"roll" => %{"dice_count" => dice_count, "wound" => wound, "stunt" => stunt}}, socket) do
-    pool =
-      SuccessDicePool.create(
-        String.to_integer(dice_count),
-        stunt: String.to_integer(stunt),
-        wound: String.to_integer(wound)
-      )
+  def handle_event("validate", %{"success_dice_pool" => dice_pool_params}, %{assigns: %{dice_pool: dice_pool}} = socket) do
+    changeset =
+      dice_pool
+      |> Dice.SuccessDicePool.changeset(dice_pool_params)
+      |> Map.put(:action, :validate)
 
-    case Tables.add_roll(socket.assigns.table, socket.assigns.player, pool) do
-      %Table{} = table ->
-        ExaultedRollerWeb.Endpoint.broadcast_from(self(), table_topic(socket), "roll_update", nil)
-        {:noreply, assign(socket, :table, table)}
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
 
-      _ ->
-        {:noreply, socket}
+  @impl true
+  def handle_event("validate", params, socket) do
+    Logger.debug("params = #{inspect params}")
+    Logger.debug("socket.assigns = #{inspect socket.assigns}")
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("roll", %{"success_dice_pool" => dice_pool_params}, socket) do
+    Logger.debug("dice_pool_params = #{inspect dice_pool_params}")
+
+    case Dice.SuccessDicePool.create(dice_pool_params) do
+      {:ok, pool} ->
+        Logger.debug("pool = #{inspect pool}")
+
+        count = Map.get(pool, :dice)
+        once_range = Map.get(pool, :reroll_once)
+        until_none_range = Map.get(pool, :reroll_none)
+        attrs =
+          pool
+          |> Map.delete(:dice)
+          |> Map.delete(:reroll_once)
+          |> Map.delete(:reroll_none)
+
+        pool =
+          SuccessDicePool.create(count, Map.to_list(attrs))
+          |> SuccessDicePool.reroll(once_range, :once)
+          |> SuccessDicePool.reroll(until_none_range, :until_none)
+
+        Logger.debug("pool = #{inspect pool}")
+
+        case Tables.add_roll(socket.assigns.table, socket.assigns.player, pool) do
+          %Tables.Table{} = table ->
+            ExaultedRollerWeb.Endpoint.broadcast_from(self(), table_topic(socket), "roll_update", nil)
+            {:noreply, assign(socket, :table, table)}
+
+          _ ->
+            {:noreply, socket}
+        end
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
     end
   end
 
@@ -91,12 +134,37 @@ defmodule ExaultedRollerWeb.RollerLive do
   @impl true
   def handle_info(%{event: "roll_update"}, socket) do
     case Tables.join(uid: socket.assigns.table.uid) do
-      %Table{} = table ->
+      %Tables.Table{} = table ->
         {:noreply, assign(socket, :table, table)}
 
       _ ->
         {:noreply, socket}
     end
+  end
+
+  defp assign_table(%{} = socket) do
+    socket
+    |> assign(:table, Tables.join(uid: socket.assigns.table.uid))
+  end
+
+  defp assign_players(%{} = socket) do
+    socket
+    |> assign(:players, table_players(socket))
+  end
+
+  defp assign_dice_pool(%{} = socket) do
+    socket
+    |> assign(:dice_pool, %Dice.SuccessDicePool{})   # Dice.create_success_dice_pool
+  end
+
+  defp assign_changeset(%{assigns: %{dice_pool: dice_pool}} = socket) do
+    socket
+    |> assign(:changeset, Dice.SuccessDicePool.changeset(dice_pool, %{}))   # Dice.create_success_dice_pool
+  end
+
+  defp assign_changeset(%{} = socket) do
+    socket
+    |> assign(:changeset, Dice.SuccessDicePool.changeset(%Dice.SuccessDicePool{}, %{}))
   end
 
   defp table_topic(socket) do
