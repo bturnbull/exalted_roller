e66c97f2124fb43c9feaeae9a237f2f110dcf7fa
bturnbull <github@brianturnbull.com>
2023-02-01 19:30:26 -0500

    Deployment for fly.io

--- lib/exaulted_roller/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule ExaultedRoller.Repo do
-  use Ecto.Repo,
-    otp_app: :exaulted_roller,
-    adapter: Ecto.Adapters.Postgres
-end
---
+++ rel/overlays/bin/server.bat
@@ -0,0 +1,2 @@
+set PHX_SERVER=true
+call "%~dp0\exaulted_roller" start
