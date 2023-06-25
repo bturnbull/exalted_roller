fffa540f8288d73186e988e53dc76015ed9b8e43
bturnbull <github@brianturnbull.com>
2023-06-24 21:54:20 -0400

    Release 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExaltedRoller.MixProject do
   def project do
     [
       app: :exalted_roller,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
